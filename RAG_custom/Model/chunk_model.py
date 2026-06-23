82eef92ba0806c298355f46099b4ac36543ed17b
MircoPerna <132653588+MircoPerna99@users.noreply.github.com>
2026-06-23 21:37:31 +0200

    Added guid

--- RAG_custom/Model/chunk_model.py
+++ RAG_custom/Model/chunk_model.py
@@ -1,14 +1,14 @@
 from  datetime import datetime
 class ChunkModel():
     def __init__(self):
-        self.id : int 
+        self.id : str 
         self.chunk_index : int
         self.text: str
         self.token_count : int
         self.embedding : list[float]
         self.created_at : datetime
     
-    def init(self, id  = 0, chunk_index = 0, text = "", token_count  = 0, embedding = [] ,created_at = None):
+    def init(self, id  = "", chunk_index = 0, text = "", token_count  = 0, embedding = [] ,created_at = None):
         self.id = id
         self.chunk_index = chunk_index
         self.text = text
